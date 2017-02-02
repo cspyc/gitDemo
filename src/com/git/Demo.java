8bfadc667b0c9c5cca914d1dc9dbfcafe12cbcf9
cspyc <piyicheng001@qq.com>
2017-02-02 23:35:43 +0800

    first commit

---
+++ src/com/git/Demo.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String msg = "It's the Demo!";
+		System.out.printf(msg);
+	}
+
+}
